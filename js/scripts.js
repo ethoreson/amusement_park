1d6ee4fc4286b0e5f495c83e3e318dfb779658c7
Emilie Thoreson and Steve Zaske <steve@>
2017-06-01 11:58:33 -0700

    include height logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(function() {
-  alert("works");
+  var height = parseInt(prompt("height? (in inches)"));
+
+  if (height < 44) {
+    alert("you're too short");
+  } else if (height > 44 && <= 55) {
+    // ride 1
+  } else if (height >= 55 && height < 57) {
+    // ride 1 & 2
+  } else if (height >= 57 && height < 66) {
+    // ride 2
+  } else if (height >=66 && height < 71) {
+    // ride 2 & 3
+  } else {
+    alert("you're too tall");
+  }
 });
